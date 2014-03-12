dce1d450f4984d3b72c14557ba48c882012d4a5e
Saitek <dragonfire2073@gmail.com>
2014-03-11 21:42:13 -0400

    Working on the update checker

--- saitek/darkadaptations/UpdateInfo.java
+++ saitek/darkadaptations/UpdateInfo.java
@@ -5,34 +5,31 @@ import java.net.URL;
 
 public class UpdateInfo {
 
-	//this number will get higher for every update your mod has
+	// this number will get higher for every update your mod has
 	public static int currentVersion = 8;
 
-	//This url needs changing to your own url
+	// This url needs changing to your own url
 	public static final String versionURL = "http://build.darkserver.co.uk/job/Dark-Adaptations/";
 
 	public static String getLatestVersion() throws Exception {
-	String data = getData(versionURL);
-	return data.substring(data.indexOf("[version]") + 9, data.indexOf("[/version]"));
+		String data = getData(versionURL);
+		return data.substring(data.indexOf("[version]") + 9, data.indexOf("[/version]"));
 	}
 
 	private static String getData(String address) throws Exception {
-	URL url = new URL(address);
-	InputStream html = null;
-	html = url.openStream();
+		URL url = new URL(address);
+		InputStream html = null;
+		html = url.openStream();
 
-	int c = 0;
-	StringBuffer buffer = new StringBuffer("");
+		int c = 0;
+		StringBuffer buffer = new StringBuffer("");
 
-	while (c != -1) {
-	c = html.read();
-	buffer.append((char) c);
+		while (c != -1) {
+			c = html.read();
+			buffer.append((char) c);
+		}
+
+		return buffer.toString();
 	}
 
-	return buffer.toString();
-	}
-
-	
-	
-	
 }
